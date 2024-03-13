a246ddfef76e3d640fd17b26da74116de1f27d3c
mazd <1695165447@qq.com>
2024-03-13 15:01:35 +0800

    toRefs 使用

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,47 +1,29 @@
 <template>
   <div class="person">
-    <h2>姓名：{{ student.name }}</h2>
-    <h2>年龄：{{ student.age }}</h2>
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
     <button @click="setName">修改名字</button>
     <button @click="setAge">修改年龄</button>
-    <button @click="showTel">查看联系方式：</button>
-    <br>
-    <h2>游戏列表：</h2>
-    <ul>
-      <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-    </ul>
-    <button @click="setFirstGame">修改第一个游戏名字</button>
   </div>
 </template>
 <script lang="ts" name='Person11' setup>
 
-import {reactive, ref} from 'vue'
+import {reactive, ref, toRefs} from 'vue'
 
 // 对象类型响应数据
-let student = ref({name: '张三2', age: 19, tel: '1380000000'})
-// 数组类型响应数据
-let games = ref([{id: '01', name: '王者'}, {id: '02', name: '原神'}, {id: '03', name: '三国'},])
+let student = reactive({name: '张三2', age: 19})
 
-let obj = reactive({x:999})
-console.log(student)
-console.log(obj)
-
-
+let {name,age} = toRefs(student)
+console.log(name)
+console.log(age)
 function setName() {
-    student.value.name = 'zhang-san'
+    name.value += '~'
 }
 
 function setAge() {
-    student.value.age += 1
+    age.value += 1
 }
 
-function showTel() {
-    alert(student.value.tel)
-}
-function setFirstGame(){
-    games.value[0].name = '英雄联盟'
-    console.log(games[0])
-}
 
 </script>
 <style scoped>
@@ -56,7 +38,8 @@ function setFirstGame(){
 button {
   margin: 0 5px;
 }
-li{
+
+li {
   font-size: 20px;
 }
 </style>
