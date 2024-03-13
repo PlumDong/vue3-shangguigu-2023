090b125a1828b09b8f5dec5eb6afe482d41350df
mazd <1695165447@qq.com>
2024-03-13 10:02:16 +0800

    ref创建基本类型的响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -8,18 +8,19 @@
   </div>
 </template>
 <script lang="ts" name='Person11' setup>
-// begin 此时name，age，tel都不是响应式的
-let name = '张三'
-let age = 18
+import {ref} from 'vue'
+
+let name = ref('张三')
+let age = ref(18)
 let tel = '1380000000'
 
 // end
 function setName() {
-    name = 'zhang-san'
+    name.value = 'zhang-san'
 }
 
 function setAge() {
-    age += 1
+    age.value += 1
 }
 
 function showTel() {
