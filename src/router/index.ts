8eda1dd2b1e22d1c0713f20b33e63d9875d9ffd8
mazd <1695165447@qq.com>
2024-03-19 14:10:43 +0800

    路由-路由器工作模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 // 创建一个路由器，并暴露出去
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router';
 import Mine from "@/pages/Mine.vue";
 import Home from "@/pages/Home.vue";
 import About from "@/pages/About.vue";
@@ -23,8 +23,9 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),    // 路由器的工作模式
+    history: createWebHashHistory(),    // 路由器的工作模式
     routes                          // 路由规则
 });
-
+// history 模式  history: createWebHashHistory()   toC项目常用，例如b站，优点：没有#号，缺点：nginx 需要配置 try_files
+// hash 模式  history: createWebHashHistory()      后台管理项目常用，优点：稳定性好，缺点：路径带有井号，
 export default router;
