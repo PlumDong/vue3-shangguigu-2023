a21716c2a216a6130a7a928305104a296561ce1e
mazd <1695165447@qq.com>
2024-03-26 16:28:19 +0800

    store 组合式写法

--- src/store/LoveTalk.ts
+++ src/store/LoveTalk.ts
@@ -3,17 +3,29 @@ import {reactive} from "vue";
 import axios from "axios";
 import {nanoid} from "nanoid";
 
-const useLoveTalkStore = defineStore('LoveTalkStore', {
-    actions: {
-        async getATalk() {
-            let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
-            this.talkList.unshift({id: nanoid(), title})
-        }
-    },
-    state():{talkList:{id:string,title:string}[]} {
-        return {
-            talkList: JSON.parse(localStorage.getItem('loveTalkStore')??'[]' )
-        }
-    },
+// const useLoveTalkStore = defineStore('LoveTalkStore', {
+//     actions: {
+//         async getATalk() {
+//             let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+//             this.talkList.unshift({id: nanoid(), title})
+//         }
+//     },
+//     state():{talkList:{id:string,title:string}[]} {
+//         return {
+//             talkList: JSON.parse(localStorage.getItem('loveTalkStore')??'[]' )
+//         }
+//     },
+// });
+const useLoveTalkStore = defineStore('LoveTalkStore', () => {
+    let talkList = reactive(JSON.parse(localStorage.getItem('loveTalkStore')??'[]' ))
+
+    async function getATalk() {
+        let {data: {content: title}} = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+        talkList.unshift({id: nanoid(), title})
+    }
+    return {
+        talkList,
+        getATalk,
+    }
 });
 export default useLoveTalkStore
