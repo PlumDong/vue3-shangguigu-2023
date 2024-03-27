15fca45d5167613e37a1565cdbed4399b6dbce2c
mazd <1695165447@qq.com>
2024-03-27 10:16:59 +0800

    组件通信

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,45 @@
 <template>
-  <div class="app">
-    <Person></Person>
-    <br>
-    <LoveTalk></LoveTalk>
+  <div class="container-fluid wraper">
+    <h1 class="title">
+      Vue3 组件间通信
+    </h1>
+    <hr>
+    <div class="row">
+      <div class="col-xs-3 col-md-3 col-lg-3 col-xl-3">
+        <!-- 导航区 -->
+        <router-link active-class="active" class="list-group-item" to="/props">1. props</router-link>
+        <router-link active-class="active" class="list-group-item" to="/event">2. 自定义事件</router-link>
+        <router-link active-class="active" class="list-group-item" to="/mitt">3. mitt</router-link>
+        <router-link active-class="active" class="list-group-item" to="/model">4. v-model</router-link>
+        <router-link active-class="active" class="list-group-item" to="/attrs">5. $attrs</router-link>
+        <router-link active-class="active" class="list-group-item" to="/ref-parent">6. <span class="small">$refs、$parent</span></router-link>
+        <router-link active-class="active" class="list-group-item" to="/provide-inject">7. provide、inject</router-link>
+        <router-link active-class="active" class="list-group-item" to="/pinia">8. pinia</router-link>
+        <router-link active-class="active" class="list-group-item" to="/slot">9. slot</router-link>
+      </div>
+      <div class="col-xs-9 col-md-9 col-lg-9 col-xl-9">
+        <div class="panel-body">
+          <!-- 占位一个展示区 -->
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-
-<script lang="ts" setup name="App">
-
-import Person from "@/pages/Person.vue";
-import LoveTalk from "@/pages/LoveTalk.vue";
+<script setup lang="ts" name="App">
 </script>
 
 <style>
-.app {
-  font-family: Arial, sans-serif;
-  max-width: 800px;
-  margin: 0 auto;
+.wraper .title {
   padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  min-width: 610px;
 }
-
-.navigate {
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 20px;
+.wraper .small{
+  font-size: 15px;
 }
-
-.navigate a {
-  text-decoration: none;
-  padding: 5px 10px;
-  border-radius: 5px;
-  background-color: #f0f0f0;
-}
-
-.navigate a:hover {
-  background-color: #e0e0e0;
-}
-
-.main-content {
-  background-color: #f9f9f9;
-  padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-.active {
-  font-weight: bold;
-  color: #ffd500;
+.wraper .list-group-item {
+  min-width: 230px;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,50 +1,52 @@
-// 创建一个路由器，并暴露出去
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router';
-import Mine from "@/pages/Mine.vue";
-import Home from "@/pages/Home.vue";
-import About from "@/pages/About.vue";
-import Detail from "@/pages/Detail.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Props from '@/pages/01_props/Father.vue'
+import Event from '@/pages/02_custom-event/Father.vue'
+import Bus from '@/pages/03_mitt/Father.vue'
+import Model from '@/pages/04_v-model/Father.vue'
+import AttrsListeners from '@/pages/05_$attrs/Father.vue'
+import RefChildrenParent from '@/pages/06_$refs-$parent/Father.vue'
+import ProvideInject from '@/pages/07_provide-inject/Father.vue'
+import Pinia from '@/pages/08_pinia/Father.vue'
+import Slot from '@/pages/09_slot/Father.vue'
 
-const routes = [
-    {
-        path: '/home',
-        name: '首页',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: '关于',
-        component: About,
-        children: [
-            {
-                path: 'detail',
-                name: '详情',
-                component: Detail,
-                // 第一种写法，将路由收到的所有params参数作为props传给路由组件
-                // props:true,
-                // 第二种写法，函数写法，可以自己决定将什么作为props给路由组件
-                props(route:any) {
-                    return route.query
-                }
-                // 第三种写法，对象写法，可以自己决定将什么作为props给路由组件
-            }
-        ]
-    },
-    {
-        path: '/mine',
-        name: '我的',
-        component: Mine
-    },
-    {
-        path: '/',
-        redirect:'/home'
-    },
-];
-
-const router = createRouter({
-    history: createWebHistory(),    // 路由器的工作模式
-    routes                          // 路由规则
-});
-// history 模式  history: createWebHashHistory()   toC项目常用，例如b站，优点：没有#号，缺点：nginx 需要配置 try_files
-// hash 模式  history: createWebHashHistory()      后台管理项目常用，优点：稳定性好，缺点：路径带有井号，
-export default router;
+export default createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/props',
+			component: Props
+		},
+		{
+			path: '/event',
+			component: Event
+		},
+		{
+			path: '/mitt',
+			component: Bus
+		},
+		{
+			path: '/model',
+			component: Model
+		},
+		{
+			path: '/attrs',
+			component: AttrsListeners
+		},
+		{
+			path: '/ref-parent',
+			component: RefChildrenParent
+		},
+		{
+			path: '/provide-inject',
+			component: ProvideInject
+		},
+		{
+			path: '/pinia',
+			component: Pinia
+		},
+		{
+			path: '/slot',
+			component: Slot
+		},
+	]
+})
